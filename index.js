5a2051b814740c791d213fbc5033d17f2abf17fd
daniel-ciupek <danter005@o2.pl>
2025-11-07 20:27:39 +0100

    toggle light/dark mode

--- index.js
+++ index.js
@@ -81,3 +81,33 @@ const iScroll = new InfiniteScroll(
   document.querySelector(".container"),
   document.querySelector(".loader-box")
 );
+
+
+
+
+
+
+
+class DarkMode {
+    constructor() {
+        this.button = document.querySelector("#checkbox");
+        this.init();
+        this.theme = localStorage.getItem("theme");
+    }
+
+    init() {
+        if (this.theme){
+            document.documentElement.setAttribute("data-theme", this.theme);
+
+            if (this.theme === "dark") this.button.checked = true;
+        }
+        this.button.addEventListener("change", this.switchTheme);
+    }
+    switchTheme = (e) => {
+        let theme = e.target.checked ? "dark" : "light";
+        document.documentElement.setAttribute("data-theme", theme);
+        localStorage.setItem("theme", theme);
+    }
+}
+
+const darkMode = new DarkMode();
